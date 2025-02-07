e9aeb94214e848f565c0c778c7b090b68bf54999
Wagner <julian57280@hotmail.fr>
2025-02-07 16:26:02 +0100

    Roger enfoiré

---
+++ exercices.js
@@ -0,0 +1,8 @@
+// Sélectionne le titre <h1> et le stocke dans une constante
+const titre = document.querySelector("h1");
+
+// Affiche le texte contenu dans le noeud HTML du titre
+console.log(titre.textContent);
+
+const mot = document.querySelector("#blague_de_roger_le_sorcier");
+mot.remove();
